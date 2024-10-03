b588e7d45a3488462c0080421425e78ce61d7164
leopc17 <LeonardoEAF45@gmail.com>
2024-10-03 19:45:17 -0300

    criação da classe Pilha

---
+++ src/structures/Pilha.java
@@ -0,0 +1,46 @@
+package structures;
+
+import interfaces.Filme_IF;
+import interfaces.Pilha_IF;
+
+public class Pilha implements Pilha_IF {
+
+    private final ListaEncadeada lista;
+
+    public Pilha() {
+        this.lista = new ListaEncadeada();
+    }
+
+    @Override
+    public Filme_IF pop() throws Exception {
+        if (lista.isEmpty()) {
+            throw new Exception("Não é possivel remover elementos de uma pilha vazia");
+        }
+
+        long id = lista.tail().getID();
+        return lista.remove(id);
+    }
+
+    @Override
+    public void push(Filme_IF elemento) {
+        lista.insert(elemento);
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return lista.isEmpty();
+    }
+
+    @Override
+    public Filme_IF top() throws Exception {
+        if (lista.isEmpty()) {
+            throw new Exception("A pilha está vazia");
+        }
+
+        return lista.tail();
+    }
+
+    public void print() {
+        lista.ReversePrint();
+    }
+}
