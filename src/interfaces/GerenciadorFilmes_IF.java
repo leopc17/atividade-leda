3a64cc344f359d32fbdb84ef405d992391fb0bea
leopc17 <LeonardoEAF45@gmail.com>
2024-10-19 13:14:08 -0300

    criação de interface auxiliar

---
+++ src/interfaces/GerenciadorFilmes_IF.java
@@ -0,0 +1,8 @@
+package interfaces;
+
+public interface GerenciadorFilmes_IF {
+    void inserir(Filme_IF elemento);
+    Filme_IF buscar(int id) throws Exception;
+    Filme_IF remover(int id) throws Exception;
+    givoid ordenar();
+}
