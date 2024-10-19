c5943d53c4b092b17a53215e9c0e1c3eb42e0999
leopc17 <LeonardoEAF45@gmail.com>
2024-10-19 13:16:13 -0300

    correção na interface

--- src/interfaces/GerenciadorFilmes_IF.java
+++ src/interfaces/GerenciadorFilmes_IF.java
@@ -4,5 +4,5 @@ public interface GerenciadorFilmes_IF {
     void inserir(Filme_IF elemento);
     Filme_IF buscar(int id) throws Exception;
     Filme_IF remover(int id) throws Exception;
-    givoid ordenar();
+    void ordenar();
 }
