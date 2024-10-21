4a953c0f6d5976d9241ac13d75c3faacaf6404a9
leopc17 <LeonardoEAF45@gmail.com>
2024-10-21 15:29:58 -0300

    criação básica do gerenciador de filmes com ABB

--- src/utils/GerenciadorFilmesABB.java
+++ src/utils/GerenciadorFilmesABB.java
@@ -1,27 +1,33 @@
 package utils;
 
+import interfaces.BST_IF;
 import interfaces.Filme_IF;
 import interfaces.GerenciadorFilmes_IF;
+import structures.ABB;
 
 public class GerenciadorFilmesABB implements GerenciadorFilmes_IF {
 
+    private final BST_IF abb = new ABB();
+
     @Override
     public void inserir(Filme_IF elemento) {
-
+        abb.insert(elemento);
     }
 
     @Override
     public Filme_IF buscar(int id) throws Exception {
-        return null;
+        return abb.search(id);
     }
 
     @Override
     public Filme_IF remover(int id) throws Exception {
-        return null;
+        return abb.remove(id);
     }
 
     @Override
-    public void ordenar() {
-
+    public Filme_IF[] ordenar() {
+        Filme_IF[] vetor = abb.order();
+        Ordenacao.mergeSort(vetor);
+        return vetor;
     }
 }
