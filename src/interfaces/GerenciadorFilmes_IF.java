d534f12061ed2093618777c83af344a279604a4f
leopc17 <LeonardoEAF45@gmail.com>
2024-10-21 15:26:55 -0300

    alteração na interface auxiliar

--- src/interfaces/GerenciadorFilmes_IF.java
+++ src/interfaces/GerenciadorFilmes_IF.java
@@ -1,8 +1,12 @@
 package interfaces;
 
 public interface GerenciadorFilmes_IF {
+
     void inserir(Filme_IF elemento);
+
     Filme_IF buscar(int id) throws Exception;
+
     Filme_IF remover(int id) throws Exception;
-    void ordenar();
+
+    Filme_IF[] ordenar() throws Exception;
 }
