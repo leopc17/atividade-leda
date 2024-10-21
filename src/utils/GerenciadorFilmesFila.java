6abfb9076e09c4360d6c1b1620ed53aad7987249
leopc17 <LeonardoEAF45@gmail.com>
2024-10-21 15:28:22 -0300

    criação do método ordenar

--- src/utils/GerenciadorFilmesFila.java
+++ src/utils/GerenciadorFilmesFila.java
@@ -1,5 +1,6 @@
 package utils;
 
+import entities.Filme;
 import interfaces.Fila_IF;
 import interfaces.Filme_IF;
 import interfaces.GerenciadorFilmes_IF;
@@ -7,11 +8,7 @@ import structures.Fila;
 
 public class GerenciadorFilmesFila implements GerenciadorFilmes_IF {
 
-    private Fila_IF fila;
-
-    public GerenciadorFilmesFila() {
-        this.fila = new Fila();
-    }
+    private final Fila fila = new Fila();
 
     @Override
     public void inserir(Filme_IF elemento) {
@@ -62,7 +59,16 @@ public class GerenciadorFilmesFila implements GerenciadorFilmes_IF {
     }
 
     @Override
-    public void ordenar() {
+    public Filme_IF[] ordenar() throws Exception {
+        int t = fila.getSize();
+        Filme_IF[] vetor = new Filme[t];
 
+        Fila aux = fila;
+        for (int i = 0; i < t; i++) {
+            vetor[i] = aux.dequeue();
+        }
+
+        Ordenacao.mergeSort(vetor);
+        return vetor;
     }
 }
