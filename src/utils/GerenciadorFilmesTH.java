5c15bf9426f37e118ca910fea2c0596e7b852dcf
leopc17 <LeonardoEAF45@gmail.com>
2024-11-01 16:42:18 -0300

    criação do gerenciador de filmes com tabela hash

--- src/utils/GerenciadorFilmesTH.java
+++ src/utils/GerenciadorFilmesTH.java
@@ -2,26 +2,33 @@ package utils;
 
 import interfaces.Filme_IF;
 import interfaces.GerenciadorFilmes_IF;
+import structures.TabelaHash;
 
 public class GerenciadorFilmesTH implements GerenciadorFilmes_IF {
 
+    private TabelaHash t;
+
+    public GerenciadorFilmesTH(int tam) {
+        this.t = new TabelaHash(tam);
+    }
+
     @Override
     public void inserir(Filme_IF elemento) {
-
+        t.insert(elemento);
     }
 
     @Override
     public Filme_IF buscar(int id) throws Exception {
-        return null;
+        return t.search(id);
     }
 
     @Override
     public Filme_IF remover(int id) throws Exception {
-        return null;
+        return t.remove(id);
     }
 
     @Override
     public Filme_IF[] ordenar() {
-        return new Filme_IF[0];
+        return t.toArray();
     }
 }
