f3eb475f76a8266ce1b3626348032007e9c8aa0a
leopc17 <LeonardoEAF45@gmail.com>
2024-11-02 12:02:47 -0300

    correção na interface auxiliar

--- src/interfaces/GerenciadorFilmes_IF.java
+++ src/interfaces/GerenciadorFilmes_IF.java
@@ -4,9 +4,9 @@ public interface GerenciadorFilmes_IF {
 
     void inserir(Filme_IF elemento);
 
-    Filme_IF buscar(int id) throws Exception;
+    Filme_IF buscar(long id) throws Exception;
 
-    Filme_IF remover(int id) throws Exception;
+    Filme_IF remover(long id) throws Exception;
 
     Filme_IF[] ordenar() throws Exception;
 }
